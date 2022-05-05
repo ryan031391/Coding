4cad8a3f6d9853422adf2d123f1cf7100e572f65
guocong <ryp0313@gmail.com>
2022-05-05 14:49:48 -0700

    5/05

---
+++ 502_508/Fizz Buzz.jsx
@@ -0,0 +1,51 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+// Example 2:
+
+// Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"]
+// Example 3:
+
+// Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+// Constraints:
+
+// 1 <= n <= 104
+
+
+
+// # @param {Integer} n
+// # @return {String[]}
+// def fizz_buzz(n)
+//     (1..n).to_a.map do |num|
+//         if is_fizz(num) && is_buzz(num)
+//             "FizzBuzz"
+//         elsif is_fizz(num)
+//             "Fizz"
+//         elsif is_buzz(num)
+//             "Buzz"
+//         else
+//             "#{num}"
+//         end
+//     end
+// end
+
+// def is_fizz(num)
+//     num % 3 == 0
+// end
+
+// def is_buzz(num)
+//     num % 5 == 0
+// end
