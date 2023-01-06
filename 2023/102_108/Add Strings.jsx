a7400e4930822b01976a3390708861259efe8801
guocong <ryp0313@gmail.com>
2023-01-05 17:39:59 -0800

    1/05

---
+++ 2023/102_108/Add Strings.jsx
@@ -0,0 +1,78 @@
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+ 
+
+// Example 1:
+
+// Input: num1 = "11", num2 = "123"
+// Output: "134"
+// Example 2:
+
+// Input: num1 = "456", num2 = "77"
+// Output: "533"
+// Example 3:
+
+// Input: num1 = "0", num2 = "0"
+// Output: "0"
+ 
+
+// Constraints:
+
+// 1 <= num1.length, num2.length <= 104
+// num1 and num2 consist of only digits.
+// num1 and num2 don't have any leading zeros except for the zero itself.
+
+
+
+// # @param {String} num1
+// # @param {String} num2
+// # @return {String}
+// def add_strings(num1, num2)
+//     carry = 0
+//     result_string = ""
+//     ptr = 0
+//     while num1.length - ptr - 1 >= 0 and num2.length - ptr - 1 >= 0
+//         acc = num1[num1.length-ptr-1].to_i + num2[num2.length-ptr-1].to_i + carry
+//         if acc > 9 
+//             carry = 1
+//             acc -= 10
+//         else
+//             carry = 0
+//         end
+//         ptr += 1
+//         result_string.prepend(acc.to_s)
+//     end
+    
+//     while num1.length - ptr - 1 >= 0
+//         acc = num1[num1.length-ptr-1].to_i + carry
+//         if acc > 9 
+//             carry = 1
+//             acc -= 10
+//         else
+//             carry = 0
+//         end
+//         ptr += 1
+//         result_string.prepend(acc.to_s)
+//     end
+    
+//     while num2.length - ptr - 1 >= 0
+//         acc = num2[num2.length-ptr-1].to_i + carry
+//         if acc > 9 
+//             carry = 1
+//             acc -= 10
+//         else
+//             carry = 0
+//         end
+//         ptr += 1
+//         result_string.prepend(acc.to_s)
+//     end
+    
+//     if carry == 1
+//         result_string.prepend("1")
+//     end
+    
+//     return result_string
+// end
+
