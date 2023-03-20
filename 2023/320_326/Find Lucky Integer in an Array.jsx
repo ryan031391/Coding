ad293d429f031ff26bd38f7c98c1e396affcb704
guocong <ryp0313@gmail.com>
2023-03-20 16:27:22 -0700

    3/20 2

---
+++ 2023/320_326/Find Lucky Integer in an Array.jsx
@@ -0,0 +1,37 @@
+// Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+// Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+ 
+
+// Example 1:
+
+// Input: arr = [2,2,3,4]
+// Output: 2
+// Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+// Example 2:
+
+// Input: arr = [1,2,2,3,3,3]
+// Output: 3
+// Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+// Example 3:
+
+// Input: arr = [2,2,2,3,3]
+// Output: -1
+// Explanation: There are no lucky numbers in the array.
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 500
+// 1 <= arr[i] <= 500
+
+
+
+// # @param {Integer[]} arr
+// # @return {Integer}
+// def find_lucky(arr)
+//   new_hash = {}
+//   arr.uniq.select{|v| new_hash[v] = arr.count(v) if v == arr.count(v)}
+//   new_hash.empty? ? -1 : new_hash.keys.max
+// end
