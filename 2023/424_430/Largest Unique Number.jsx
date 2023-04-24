bae8dbe71d97006d21713d4bb28784d6dfede26f
guocong <ryp0313@gmail.com>
2023-04-24 16:42:06 -0700

    4/24

---
+++ 2023/424_430/Largest Unique Number.jsx
@@ -0,0 +1,28 @@
+// Given an integer array nums, return the largest integer that only occurs once. If no integer occurs once, return -1.
+
+ 
+
+// Example 1:
+
+// Input: nums = [5,7,3,9,4,9,8,3,1]
+// Output: 8
+// Explanation: The maximum integer in the array is 9 but it is repeated. The number 8 occurs only once, so it is the answer.
+// Example 2:
+
+// Input: nums = [9,9,8,8]
+// Output: -1
+// Explanation: There is no number that occurs only once.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 2000
+// 0 <= nums[i] <= 1000
+
+
+
+// def largest_unique_number(a)
+//     h = a.each_with_object({}) { |e, h| h[e] = h[e].nil? ? true : false }
+//     h.select! { |k, v| v == true }
+//     h.keys.empty? ? -1 : h.keys.max
+// end
